539b92286b33afe5d766c1f16778066499f83307
Jenifer sanchez <jenifersanchezaws@gmail.com>
2025-05-12 18:24:52 -0400

    modify terraform rds

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "mysql_credito" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
-  name                 = var.db_name
+  db_name                 = var.db_name
   username             = var.db_username
   password             = var.db_password
   publicly_accessible  = true
